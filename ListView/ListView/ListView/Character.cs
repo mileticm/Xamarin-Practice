65c4df46d34cb8d10bdad1701d6e86885644ddf5
mileticm <mmiletic2@foi.hr>
2017-04-12 18:15:37 +0200

    Character class
    New class

---
+++ ListView/ListView/ListView/Character.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListView
+{
+    class Character
+    {
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public string ImageUrl { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}", Name, Species);
+        }
+
+        public static IList<Character> Characters
+        {
+            get
+            {
+                return new List<Character>
+                {
+                    new Character
+                    {
+                        Name = "Frodo",
+                        Species = "Hobbit"
+                    },
+                    new Character
+                    {
+                        Name = "Aragorn",
+                        Species = "Human"
+                    }
+                };
+            }
+        }
+    }
+}
